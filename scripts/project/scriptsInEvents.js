d61bbcf20a5ba7a662c6f548c7964ce528995a10
Anjela Gogichaeva <103134246+AnjGogich@users.noreply.github.com>
2024-12-11 17:01:40 +0300

    Add files via upload

---
+++ scripts/project/scriptsInEvents.js
@@ -0,0 +1,39 @@
+
+
+
+const scriptsInEvents = {
+
+	async списоксобытий1_Event1(runtime, localVars)
+	{
+		начало
+	},
+
+	async списоксобытий1_Event3(runtime, localVars)
+	{
+		движение
+	},
+
+	async списоксобытий1_Event6(runtime, localVars)
+	{
+		столкновение
+	},
+
+	async списоксобытий1_Event11(runtime, localVars)
+	{
+		счет
+	},
+
+	async списоксобытий1_Event13(runtime, localVars)
+	{
+		заднийфон
+	},
+
+	async списоксобытий1_Event19(runtime, localVars)
+	{
+		препятствие
+	}
+
+};
+
+self.C3.ScriptsInEvents = scriptsInEvents;
+
